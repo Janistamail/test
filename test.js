a5fb2151780718b2cf397b31b3f41d43b3150915
Janistamail <janista.shrw@gmail.com>
2022-07-25 16:56:19 +0700

    test

--- test.js
+++ test.js
@@ -1,9 +1,7 @@
-// let n1 = 0;
-// let n2 = 1;
-let arr = [0];
+let arr = [];
 const fibonacci = (term) => {
-  for (let i = 1; i < term; i++) {
-    if (i === 1) {
+  for (let i = 0; i < term; i++) {
+    if (i <= 1) {
       arr.push(i);
     } else {
       arr.push(arr[i - 2] + arr[i - 1]);
